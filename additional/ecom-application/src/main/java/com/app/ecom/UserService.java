fe65c380c1908f2f19c01c3538510de984aebc4e
Faisal Memon <faisal.memon1991@gmail.com>
2025-03-05 14:00:18 +0530

    Making User Persistent

--- additional/ecom-application/src/main/java/com/app/ecom/UserService.java
+++ additional/ecom-application/src/main/java/com/app/ecom/UserService.java
@@ -1,5 +1,6 @@
 package com.app.ecom;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -7,32 +8,31 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
-    private List<User> userList = new ArrayList<>();
+    private final UserRepository userRepository;
+//    private List<User> userList = new ArrayList<>();
     private Long nextId = 1L;
 
     public List<User> fetchAllUsers(){
-        return userList;
+        return userRepository.findAll();
     }
 
     public void addUser(User user){
-        user.setId(nextId++);
-        userList.add(user);
+//        user.setId(nextId++);
+        userRepository.save(user);
     }
 
     public Optional<User> fetchUser(Long id) {
-        return userList.stream()
-                .filter(user -> user.getId().equals(id))
-                .findFirst();
+        return userRepository.findById(id);
     }
 
     public boolean updateUser(Long id, User updatedUser) {
-        return userList.stream()
-                .filter(user -> user.getId().equals(id))
-                .findFirst()
+        return userRepository.findById(id)
                 .map(existingUser -> {
                     existingUser.setFirstName(updatedUser.getFirstName());
                     existingUser.setLastName(updatedUser.getLastName());
+                    userRepository.save(existingUser);
                     return true;
                 }).orElse(false);
     }
