24f640829fecaeb0a8721e8c3e6bfc6c8f6f0d3b
Faisal Memon <faisal.memon1991@gmail.com>
2025-03-06 18:37:45 +0530

    Allowing Users to Place Order

--- additional/ecom-application/src/main/java/com/app/ecom/model/Order.java
+++ additional/ecom-application/src/main/java/com/app/ecom/model/Order.java
@@ -8,11 +8,13 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-@Entity
+@Entity(name = "orders")
 @Data
 @NoArgsConstructor
-public class CartItem {
+public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -21,12 +23,13 @@ public class CartItem {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @ManyToOne
-    @JoinColumn(name = "product_id", nullable = false)
-    private Product product;
+    private BigDecimal totalAmount;
 
-    private Integer quantity;
-    private BigDecimal price;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status = OrderStatus.PENDING;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> items = new ArrayList<>();
 
     @CreationTimestamp
     private LocalDateTime createdAt;
--- additional/ecom-application/src/main/java/com/app/ecom/service/CartService.java
+++ additional/ecom-application/src/main/java/com/app/ecom/service/CartService.java
@@ -73,4 +73,10 @@ public class CartService {
                 .map(cartItemRepository::findByUser)
                 .orElseGet(List::of);
     }
+
+    public void clearCart(String userId) {
+        userRepository.findById(Long.valueOf(userId)).ifPresent(
+                cartItemRepository::deleteByUser
+                );
+    }
 }
