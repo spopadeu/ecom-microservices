eec73aac54538ecb90661ecf0451e0c534d64f4a
Faisal Memon <faisal.memon1991@gmail.com>
2025-03-16 16:00:02 +0530

    Spring Cloud Config Server

---
+++ additional/config-mgmt/configdemo/src/main/java/com/info/configdemo/BuildInfoController.java
@@ -0,0 +1,27 @@
+package com.info.configdemo;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class BuildInfoController {
+
+//    @Value("${build.id:default}")
+//    private String buildId;
+//
+//    @Value("${build.version:default}")
+//    private String buildVersion;
+//
+//    @Value("${build.name:default}")
+//    private String buildName;
+
+    private BuildInfo buildInfo;
+
+    @GetMapping("/build-info")
+    public String getBuildInfo() {
+        return "Build ID: " + buildInfo.getId() + ", Version: " + buildInfo.getVersion() + ", Name: " + buildInfo.getName();
+    }
+}
