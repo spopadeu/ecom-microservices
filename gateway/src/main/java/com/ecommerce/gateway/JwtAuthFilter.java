89edcc912aa52dd040c21fee76df1d5a6f7e0b3f
Faisal Memon <faisal.memon1991@gmail.com>
2025-03-28 10:07:01 +0530

    http vs lb

--- gateway/src/main/java/com/ecommerce/gateway/JwtAuthFilter.java
+++ gateway/src/main/java/com/ecommerce/gateway/JwtAuthFilter.java
@@ -8,7 +8,7 @@ import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
-@Component
+//@Component
 public class JwtAuthFilter implements WebFilter {
 
     @Override
