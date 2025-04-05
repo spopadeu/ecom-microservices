3fb32f68911227d3a899500f1cfc7397f1a451f2
Faisal Memon <faisal.memon1991@gmail.com>
2025-04-05 17:18:46 +0530

    Retry Pattern API Gateway

--- product/src/main/java/com/ecommerce/product/controllers/ProductController.java
+++ product/src/main/java/com/ecommerce/product/controllers/ProductController.java
@@ -17,6 +17,15 @@ public class ProductController {
 
     private final ProductService productService;
 
+    @GetMapping("/simulate")
+    public ResponseEntity<String> simulateFailure(
+            @RequestParam(defaultValue = "false") boolean fail) {
+        if (fail) {
+            throw new RuntimeException("Simulated Failure For Testing");
+        }
+        return ResponseEntity.ok("Product Service is OK");
+    }
+
     @PostMapping
     public ResponseEntity<ProductResponse> createProduct(@RequestBody ProductRequest productRequest) {
         return new ResponseEntity<ProductResponse>(productService.createProduct(productRequest),
