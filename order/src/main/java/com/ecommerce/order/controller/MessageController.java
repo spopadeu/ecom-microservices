0a4102929be7becf008715f95286e0b3d7c2de79
Faisal Memon <faisal.memon1991@gmail.com>
2025-04-05 18:19:43 +0530

    Implementing RateLimiting

--- order/src/main/java/com/ecommerce/order/controller/MessageController.java
+++ order/src/main/java/com/ecommerce/order/controller/MessageController.java
@@ -1,5 +1,7 @@
 package com.ecommerce.order.controller;
 
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +15,12 @@ public class MessageController {
     private String message;
 
     @GetMapping("/message")
+    @RateLimiter(name = "rateBreaker", fallbackMethod = "getMessageFallback")
     public String getMessage() {
         return message;
+    }
+
+    public String getMessageFallback(Exception e) {
+        return "Hello Fallback";
     }
 }
